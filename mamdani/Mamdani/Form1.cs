242ffa9d5b589b90ee24117c30d71810fddbb98b
Egorka99 <38012571+Egorka99@users.noreply.github.com>
2021-04-03 10:50:00 +0300

    add files

---
+++ mamdani/Mamdani/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SII_lr3
+{
+    public partial class Form1 : Form
+    {
+        LogicalConclusion LC;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            LC = new LogicalConclusion(country, background, consequence, X, Y, Ax, A, By, B, R);
+
+            setData();
+        }
+
+        string country = "Саудовская Аравия"; //название страны
+        string background = "Цена на нефть"; //предпосылка
+        string consequence = "Бюджет"; //следствие
+
+        string[] X = new string[] { "10$", "20$", "50$", "80$", "100$", "120$", "150$" }; //цена на нефть в долларах США
+        string[] Y = new string[] { "-3%", "-1%", "0%", "1%", "3%", "5%" }; //сбалансированность бюджета в %. 0% - сбалансированный, меньше нуля - дефицитный, больше нуля - профицитный
+
+        string[] Ax = new string[] { "Критически низкая",   //предпосылки
+                                    "Очень низкая", 
+                                    "Низкая", 
+                                    "Приемлемая", 
+                                    "Ниже среднего", 
+                                    "Средняя", 
+                                    "Выше среднего", 
+                                    "Высокая", 
+                                    "Очень высокая", 
+                                    "Неимоверно высокая" };
+
+        double[][] A = new double[][] { new double[] { 1, 0.8, 0.1, 0, 0, 0, 0 },
+                                        new double[] { 1, 1, 0.3, 0, 0, 0, 0 },
+                                        new double[] { 1, 1, 0.7, 0.2, 0, 0, 0 },
+                                        new double[] { 0, 0.3, 0.9, 0.1, 0, 0, 0 },
+                                        new double[] { 0, 0.4, 0.8, 0.2, 0, 0, 0 },
+                                        new double[] { 0, 0.1, 0.7, 1, 0.5, 0, 0 },
+                                        new double[] { 0, 0, 0.2, 0.6, 0.9, 0.1, 0 },
+                                        new double[] { 0, 0, 0.1, 0.4, 0.6, 0.6, 0.1 },
+                                        new double[] { 0, 0, 0, 0.2, 0.5, 0.9, 0.5 },
+                                        new double[] { 0, 0, 0, 0.1, 0.3, 0.8, 1 } };
+
+        string[] By = new string[] { "Очень дефицитный",   //следствия
+                                    "Дефицитный", 
+                                    "Сбалансированный", 
+                                    "Профицитный", 
+                                    "Очень профицитный" };
+
+        double[][] B = new double[][] { new double[] { 1, 0.7, 0, 0, 0, 0 },
+                                        new double[] { 0.8, 1, 0.1, 0, 0, 0 },
+                                        new double[] { 0, 0.5, 1, 0.5, 0, 0 },
+                                        new double[] { 0, 0, 0.1, 0.9, 0.9, 0.3 },
+                                        new double[] { 0, 0, 0, 0.2, 0.7, 1 } };
+
+        string[][] R = new string[][] { new string[] { "Критически низкая", "Очень дефицитный" }, //правила
+                                        new string[] { "Очень низкая", "Очень дефицитный" },
+                                        new string[] { "Низкая", "Дефицитный" },
+                                        new string[] { "Приемлемая", "Дефицитный" },
+                                        new string[] { "Ниже среднего", "Сбалансированный" },
+                                        new string[] { "Средняя", "Сбалансированный" },
+                                        new string[] { "Выше среднего", "Профицитный" },
+                                        new string[] { "Высокая", "Профицитный" },
+                                        new string[] { "Очень высокая", "Очень профицитный" },
+                                        new string[] { "Неимоверно высокая", "Очень профицитный" } };
+
+        private void setData()
+        {
+            for (int i = 0; i < Ax.Length; i++)
+                oilPriceComboBox.Items.Add(Ax[i]);
+            oilPriceComboBox.SelectedIndex = 5;
+
+        }
+
+        private void showRulesButton_Click(object sender, EventArgs e)
+        {
+            rulesListBox.Visible = true;
+            hideRulesButton.Visible = true;
+
+            this.Height = 676;
+
+            rulesListBox.Items.Clear();
+
+            foreach(string[] r in R)
+            {
+                string s = "Если " + background + " " + r[0] + ", то " + consequence + " " + r[1];
+                rulesListBox.Items.Add(s);
+            }
+        }
+
+        private void hideRulesButton_Click(object sender, EventArgs e)
+        {
+            rulesListBox.Visible = false;
+            hideRulesButton.Visible = false;
+
+            this.Height = 250;
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            string backgr = oilPriceComboBox.SelectedItem.ToString();
+      
+            ResultTextBox.Text = LC.Algorithm(backgr, "Mamdani");
+
+        }
+
+
+
+    }
+}
---
+++ mamdani/Mamdani/LogicalConclusion.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SII_lr3
+{
+    class LogicalConclusion
+    {
+        string countryName; //название страны
+        string background; //предпосылка
+        string consequence; //следствие
+
+        string[] X; //цена на нефть в долларах США
+        string[] Y; //сбалансированность бюджета в %. 0% - сбалансированный, меньше нуля - дефицитный, больше нуля - профицитный
+
+        string[] Ax;   //предпосылки
+
+        double[][] A;
+
+        string[] By;   //следствия
+
+        double[][] B;
+
+        string[][] R; //правила
+
+        public LogicalConclusion(string countryName, string background, string consequence, string[] X, string[] Y, string[] Ax, double[][] A, string[] By, double[][] B, string[][] R)
+        {
+            this.countryName = countryName;
+            this.background = background;
+            this.consequence = consequence;
+            this.X = X;
+            this.Y = Y;
+            this.Ax = Ax;
+            this.A = A;
+            this.By = By;
+            this.B = B;
+            this.R = R;
+        }
+
+        // первоначальная агрегацией правил
+        public string Algorithm(string backgr, string implicationType)
+        {
+            double[][][] R_i = new double[R.GetLength(0)][][];
+            for (int k = 0; k < R.GetLength(0); k++)
+            {
+                R_i[k] = new double[X.Length][];
+                for (int i = 0; i < X.Length; i++)
+                    R_i[k][i] = new double[Y.Length];
+            }
+
+            for (int k = 0; k < R.GetLength(0); k++)
+            {
+                int a_index = Array.IndexOf(Ax, R[k][0]);
+                int b_index = Array.IndexOf(By, R[k][1]);
+
+                Mamdani(A[a_index], B[b_index], R_i[k]);
+            }
+
+            double[][] R_agg = new double[X.Length][];
+            for (int i = 0; i < X.Length; i++)
+                R_agg[i] = new double[Y.Length];
+
+            for (int i = 0; i < X.Length; i++)
+                for (int j = 0; j < Y.Length; j++)
+                {
+                    double max = R_i[0][i][j];
+                    for (int k = 1; k < R.GetLength(0); k++)
+                        if (R_i[k][i][j] > max) max = R_i[k][i][j];
+                    R_agg[i][j] = max;
+                }
+
+            double[] B_ = new double[Y.Length];
+
+            int a__index = Array.IndexOf(Ax, backgr);
+
+            Max_Min(A[a__index], R_agg, B_);
+
+            string result = "";
+
+            for (int j = 0; j < Y.Length; j++)
+                result += "(" + Y[j] + ")" + "/" + B_[j] + "+";
+
+            if (result.Length > 0)
+                result = result.Remove(result.Length - 1);
+
+            return result;
+        }
+
+
+        private void Mamdani(double[] A, double[] B, double[][] R)
+        {
+            for (int i = 0; i < A.Length; i++)
+                for (int j = 0; j < B.Length; j++)
+                    R[i][j] = Min(A[i], B[j]);
+        }
+
+
+        private double Min(double a, double b)
+        {
+            if (a <= b)
+                return a;
+            else
+                return b;
+        }
+
+        private void Max_Min(double[] A, double[][] R, double[] B)
+        {
+            List<double> tmpMAX = new List<double>();
+
+            for (int j = 0; j < R[0].GetLength(0); j++)
+            {
+                tmpMAX.Clear();
+
+                for (int k = 0; k < R.GetLength(0); k++)
+                    tmpMAX.Add(Min(A[k], R[k][j]));
+
+                B[j] = tmpMAX.Max();
+            }
+        }
+
+
+
+
+    }
+}
